11d44c0f55fbdd4ae6f9e501ee0e98accdac1095
nugrahaditristyaa <93330273+nugrahaditristyaa@users.noreply.github.com>
2022-09-19 19:59:14 +0700

    Update UG4_71210677.py

--- UG4_71210677.py
+++ UG4_71210677.py
@@ -3,16 +3,16 @@ import json
 with open('mahasiswa.json','r') as file:
     data =  json.load(file)
 
-total = int(input('Masukan jumlah mahasiswa baru : '))
+total = int(input('Masukkan jumlah mahasiswa baru : '))
 
 for i in range(total):
-    nama = input('Masukan nama Anda : ')
-    hobi = int(input('Masukan Jumlah hobi : '))
+    nama = input('Masukkan nama Anda : ')
+    hobi = int(input('Masukkan Jumlah hobi : '))
     for x in range (1,hobi+1):
         tampungan = []
-        hobi = (input('Masukan Hobi ke-'+ str(x) + ' : '))
+        hobi = (input('Masukkan Hobi ke-'+ str(x) + ' : '))
         tampungan.append(hobi)
-    prestasi = input('Masukan Prestasi Anda : ')
+    prestasi = input('Masukkan Prestasi Anda : ')
     print('=== Data berhasil ditambahkan === \n')
 
 a = {"nama": nama, 'biodata' : {"hobi": hobi, "prestasi": prestasi}}
