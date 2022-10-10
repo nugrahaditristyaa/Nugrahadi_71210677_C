877ce8f591c910bb1c9cb34fb39eb8ac892f6d7a
nugrahaditristyaa <93330273+nugrahaditristyaa@users.noreply.github.com>
2022-10-10 20:12:28 +0700

    Update UG7_71210677.py

--- UG7_71210677.py
+++ UG7_71210677.py
@@ -1,37 +1,61 @@
+class Node:
+    def _init_(self,element,n):
+        self._element = element
+        self._next = n
+
 class PrefixConverter:
-    def __init__(self):
-        self.stackList = ['?']
-    
-    # method untuk menambahkan data baru
-    def push(self,data):
-        self.stackList.append(data)
-    
-    # method untuk melihat data paling atas
-    def peek(self): 
-        if self.stackList:
-            return self.stackList[-1]
-        else: 
-            return "Isi Stack Kosong"
+    def _init_(self):
+        self.stackList = []
+        self._top = -1                    
+        self._array = []               
+        self._outputArray = []
+        self._operator = {'+':1, '-':1, '*':2, '/':2, '^':3} 
 
-    # method untuk menghapus data palin atas
-    def pop(self):
-        if self.stackList:
-            data = self.stackList.pop(-1)
-            return data
-        else:
-            return "Isi Stack Kosong"
-
-    def cekValidExpression(self,expression):
-        if expression.isalpha() or expression in '1234567890':
+    def isEmpty(self):
+        if self._top == -1 :    
             return True
         else:
             return False
 
-    def infixToPrefix(self,expression):
-        
+    # method untuk menambahkan expression baru
+    def push(self,expression):
+        self.stackList.append(expression)
 
+    # method untuk melihat expression paling atas
+    def peek(self):
+        if self.stackList:
+            return self.stackList[-1]
+        else:
+            return "Isi Stack Kosong"
+
+    # method untuk menghapus expression palin atas
+    def pop(self):
+        if self.stackList:
+            expression = self.stackList.pop(-1)
+            return expression
+        else:
+            return "Isi Stack Kosong"
+
+    def isCommand(self, opp):     
+        return opp.isalpha()
+
+    def cekValidExpression(self, expression):
+        for i in expression:
+            if i.isalpha() or i == "(":
+                return False
+        return True
+
+    def infixToPrefix(self, expression):
+            if " " in expression:
+                expression = expression.split()
+
+            if not self.cekValidExpression(expression):
+                return "Expresi Infix yang anda masukkan tidak valid !!"
+            a = []
+            b = []
+            
 # Test Case
-if __name__ == '__main__'
+if __name__== '__main__':
     expresi1 = PrefixConverter()
     print(expresi1.infixToPrefix("1+2+3*4/2-1"))
     print(expresi1.infixToPrefix("A * (B + C) / D"))
